981021a422880a70674f4b29a7df574251953eb2
JasYi <44511533+JasYi@users.noreply.github.com>
2024-04-30 17:07:01 -0400

    fixing metrics.py

--- metrics.py
+++ metrics.py
@@ -4,6 +4,8 @@ import numpy as np
 import sys
 from skimage.metrics import structural_similarity as ssim
 import os
+from SSIM_PIL import compare_ssim
+from PIL import Image
 
 def PSNR(original, compressed): 
     mse = np.mean((original - compressed) ** 2) 
@@ -19,20 +21,25 @@ result_path = sys.argv[1]
 flag = False
 first_img = None
 second_img = None
+first_ssim = None
+second_ssim = None
 PSNR_agg = 0
 SSIM_agg = 0
 counter = 0
 
-for images in os.listdir(result_path):
+for images in sorted(os.listdir(result_path)):
     if flag:
         second_img = cv2.imread(result_path + "/" + images)
         second_img = cv2.resize(second_img, (256, 256))
+        second_ssim = Image.open(result_path + "/" + images)
+        second_ssim = second_ssim.resize((256, 256))
         PSNR_agg += PSNR(first_img, second_img)
-        SSIM_agg += ssim(second_img, first_img, data_range=first_img.max() - first_img.min())
+        SSIM_agg += compare_ssim(first_ssim, second_ssim)
         counter += 1
         flag = False
     else:
         first_img = cv2.imread(result_path + "/" + images)
+        first_ssim = Image.open(result_path + "/" + images)
         flag = True
 
 print(f"Average PSNR: {PSNR_agg / counter}")
